74e37f8681978a5420fa72ed444aeaaf81bf9cb5
Gabriel Seals <sealsgj@gmail.com>
2019-08-24 13:36:40 -0500

    adding index and objects

---
+++ objects.js
@@ -0,0 +1,64 @@
+// const bouncer = (age) => {
+//     if (age >= 21) {
+//         console.log('DRINK (at the bar)');
+//     } else {
+//         console.log('SHAME')
+//     }
+// }
+
+// bouncer(12);
+// bouncer(93);
+
+
+
+
+// const me = 'martin'
+// console.log(me.length);
+
+// const myObject = {
+//     name: 'john',
+//     age: '41',
+// }
+
+// console.log(myObject['name']);
+
+// console.dir(myObject);
+
+const findAndLogEmployeeStatus = (employee) => {
+    if (employee.name.charAt(0).toLowerCase() === 'c') {
+        employee.status = 'vip'
+    } else {
+        employee.status = 'peasant'
+    }
+    console.log(`${employee.name} is a total ${employee.status}`)
+}
+
+// const person = {
+//     name: 'Michael',
+//     status: ''
+// }
+
+// findAndLogEmployeeStatus(person);
+
+// findAndLogEmployeeStatus({
+//     name: 'lester',
+//     status: ''
+// }
+// )
+// findAndLogEmployeeStatus({
+//     name: prompt('Enter employee name'),
+//     status: ''
+// });
+
+let person = {
+    name: prompt('what yo name?'),
+    status: ''
+}
+findAndLogEmployeeStatus(person);
+
+
+
+
+
+
+
